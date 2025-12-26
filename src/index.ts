ad77103d71f3f63ea376e6af292e3196aca7a056
Franco Guze <franguze@13gmail.com>
2025-12-26 17:41:57 -0300

    fix/Corrected issue with server

--- src/index.ts
+++ src/index.ts
@@ -12,11 +12,10 @@ app.use(
     methods: ["GET", "POST"],
   })
 );
-//Cambiar origin con la url de front
 
-// const io = new Server(server, {
-//   cors: { origin: process.env.FRONTEND_URL || "*", methods: ["GET", "POST"] },
-// });
+const io = new Server(server, {
+  cors: { origin: process.env.FRONTEND_URL || "*", methods: ["GET", "POST"] },
+});
 app.use(express.json());
 
 const findByValue = (value: string) => {
@@ -30,74 +29,74 @@ const findByValue = (value: string) => {
   return found || false;
 };
 
-// io.on("connection", (socket) => {
-//   console.log("a user connected");
-//   socket.on("join-room", ({ room, userId }) => {
-//     socket.join(room);
-//     users.set(socket.id, userId);
-//     console.log(`${userId} entro en la sala ${room}`);
-//     console.log(socket.rooms);
-//     socket.to(room).emit("user-joined", { userId, socketId: socket.id });
-//   });
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("join-room", ({ room, userId }) => {
+    socket.join(room);
+    users.set(socket.id, userId);
+    console.log(`${userId} entro en la sala ${room}`);
+    console.log(socket.rooms);
+    socket.to(room).emit("user-joined", { userId, socketId: socket.id });
+  });
 
-//   socket.on("offer", ({ room, offer, from, to }) => {
-//     console.log("Envio de offer", { from, to });
-//     const findTo = findByValue(to);
-//     findTo && socket.to(findTo[0]).emit("offer", { offer, from: from });
-//   });
-//   socket.on("answer", ({ room, answer, to, from }) => {
-//     console.log("Envio de answer", { to });
-//     const findTo = findByValue(to);
+  socket.on("offer", ({ room, offer, from, to }) => {
+    console.log("Envio de offer", { from, to });
+    const findTo = findByValue(to);
+    findTo && socket.to(findTo[0]).emit("offer", { offer, from: from });
+  });
+  socket.on("answer", ({ room, answer, to, from }) => {
+    console.log("Envio de answer", { to });
+    const findTo = findByValue(to);
 
-//     findTo && socket.to(findTo[0]).emit("answer", { answer, from: from });
-//   });
+    findTo && socket.to(findTo[0]).emit("answer", { answer, from: from });
+  });
 
-//   socket.on("ice-candidate", ({ room, candidate, from, to }) => {
-//     console.log("LLego ice candidate");
-//     const findTo = findByValue(to);
+  socket.on("ice-candidate", ({ room, candidate, from, to }) => {
+    console.log("LLego ice candidate");
+    const findTo = findByValue(to);
 
-//     findTo && socket.to(findTo[0]).emit("ice-candidate", { candidate, from });
-//   });
+    findTo && socket.to(findTo[0]).emit("ice-candidate", { candidate, from });
+  });
 
-//   socket.on("media-update", ({ room, user, state }) => {
-//     console.log(`${user} notifica a ${room} cambio de media`);
-//     console.log({ state });
-//     socket.to(room).emit("media-update", {
-//       user,
-//       state,
-//     });
-//   });
+  socket.on("media-update", ({ room, user, state }) => {
+    console.log(`${user} notifica a ${room} cambio de media`);
+    console.log({ state });
+    socket.to(room).emit("media-update", {
+      user,
+      state,
+    });
+  });
 
-//   socket.on("disconnect", (e) => {
-//     const leavinguser = users.get(socket.id);
-//     console.log({ leavinguser, rooms: socket.rooms });
-//     users.delete(socket.id);
-//     socket.broadcast.emit("remove-user", { user: leavinguser });
-//   });
-// });
+  socket.on("disconnect", (e) => {
+    const leavinguser = users.get(socket.id);
+    console.log({ leavinguser, rooms: socket.rooms });
+    users.delete(socket.id);
+    socket.broadcast.emit("remove-user", { user: leavinguser });
+  });
+});
 
-// app.get("/a", (req, res) => {
-//   let showUsers: string[] = [];
-//   console.log(users);
-//   Array.from(users.keys()).forEach((user) => showUsers.push(user));
-//   res.send({ status: "ok", data: showUsers || "No hay" });
-// });
-// app.get("/b/:userId", (req, res) => {
-//   const userId = req.params.userId;
-//   console.log(userId);
-//   console.log(users.get(userId));
-//   res.send(userId || "nada");
-// });
-// app.get("/userExists/:userName", (req, res) => {
-//   const userName = req.params.userName;
-//   const exists = findByValue(userName);
-//   console.log(userName, "||", exists);
-//   if (!exists) {
-//     res.json({ exists, message: "This user name can be used!" });
-//   } else {
-//     res.json({ exists: true, message: "This user name cannot be used..." });
-//   }
-// });
+app.get("/a", (req, res) => {
+  let showUsers: string[] = [];
+  console.log(users);
+  Array.from(users.keys()).forEach((user) => showUsers.push(user));
+  res.send({ status: "ok", data: showUsers || "No hay" });
+});
+app.get("/b/:userId", (req, res) => {
+  const userId = req.params.userId;
+  console.log(userId);
+  console.log(users.get(userId));
+  res.send(userId || "nada");
+});
+app.get("/userExists/:userName", (req, res) => {
+  const userName = req.params.userName;
+  const exists = findByValue(userName);
+  console.log(userName, "||", exists);
+  if (!exists) {
+    res.json({ exists, message: "This user name can be used!" });
+  } else {
+    res.json({ exists: true, message: "This user name cannot be used..." });
+  }
+});
 app.get("/ping", (req, res) => {
   res.send("pong");
 });
